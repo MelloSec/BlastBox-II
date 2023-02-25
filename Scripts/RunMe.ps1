53341f93ddb41df0ffb768e7552cd36d7d464515
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-02-25 17:05:39 -0500

    Made a loader for the config scripts

---
+++ Scripts/RunMe.ps1
@@ -0,0 +1,4 @@
+Invoke-WebRequest https://github.com/MelloSec/BlastBox-II/archive/refs/heads/main.zip -o main.zip 
+Expand-Archive .\main.zip
+cd .\main\BlastBox-II-main\Scripts\
+set-executionpolicy bypass .\Choco-Loader.ps1
