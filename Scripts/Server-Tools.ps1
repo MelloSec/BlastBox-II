821ddf08f48b24da670473a0244a67a4a1f4fbe4
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-03-04 06:24:53 -0500

    Server-Tools

--- Scripts/Server-Tools.ps1
+++ Scripts/Server-Tools.ps1
@@ -1,6 +1,7 @@
 # Domain Controller
 $Domain = 'earth.sunn'
 $DSRMPassword = 'Password123!'
+
 # $NetworkID = '10.0.0.69'
 # $NewIPv4DNSServer = "8.8.8.8"
 # $InterfaceIndex = (Get-NetAdapter).ifIndex
@@ -9,18 +10,12 @@ $DSRMPassword = 'Password123!'
 # Set-NetIpAddress -InterfaceIndex $InterfaceIndex -AddressFamily "IPv4" -IPAddress "$CurrentIPv4Address" -PrefixLength 24 | Out-Null
 # Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses ("127.0.0.1", "$NewIPv4DNSServer")
 
-Write-Verbose "Installing AD DS feature..."
-
-Install-WindowsFeature -Name "AD-Domain-Services" -IncludeManagementTools
-Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
-
-
-Write-Verbose "Installing new forest: $Domain"
-    
+Write-Verbose "Installing Features and creating new forest: $Domain"    
 $Password = ConvertTo-SecureString -AsPlainText -String $DSRMPassword -Force
 $NetbiosName = $Domain.split(".")[0].ToUpper()
-
-Install-ADDSForest -DomainName "$Domain" -DomainNetbiosName "$NetbiosName" -SafeModeAdministratorPassword $Password -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -LogPath "C:\Windows\NTDS" -DomainMode "WinThreshold" -ForestMode "WinThreshold" -NoRebootOnCompletion:$false -InstallDNS:$false -Force:$true
+Install-WindowsFeature -Name "AD-Domain-Services" -IncludeManagementTools
+Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+Install-ADDSForest -DomainName "$Domain" -DomainNetbiosName "$NetbiosName" -SafeModeAdministratorPassword $Password -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -LogPath "C:\Windows\NTDS" -DomainMode "WinThreshold" -ForestMode "WinThreshold" -NoRebootOnCompletion:$true -InstallDNS:$false -Force:$true
 
 # Install boxstarter with chocolatey and basic configuration
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter –Force
@@ -33,7 +28,7 @@ choco install -y tabby
 choco install -y git
 choco install -y poshgit
 choco install -y vscode
-choco install sysinternals -y
+choco install -y sysinternals 
 
 Set-ExecutionPolicy -Bypass
 mkdir "C:\sysmon";
@@ -42,5 +37,3 @@ Expand-Archive "c:\sysmon\sysmon.zip" -DestinationPath "C:\sysmon";
 cd "c:\sysmon";
 c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\sysmon-swift.xml
 
-
-Install-ADDSForest -DomainName mellosec.sunn -InstallDNs
